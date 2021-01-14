724bd9b05aa7f341ab48b1fe7c0249a39fa0d088
Masood Imran <imran@bluegate-soft.com>
2021-01-14 14:25:16 +0000

    pushed test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+from icecream import ic
+from datetime import datetime
+import time
+
+def time_format():
+    return f'{datetime.now()}|>'
+
+def plus_five(num):
+    return num + 5
+
+def hello(user: bool):
+    if user:
+        ic()
+    else:
+        ic()
+
+ic.configureOutput(prefix=time_format, includeContext=True)
+
+ic(plus_five(4))
+ic(plus_five(5))
+
+hello(user=True)
+hello(user=False)
+
+for _ in range(3):
+    time.sleep(1)
+    ic('Hello')
